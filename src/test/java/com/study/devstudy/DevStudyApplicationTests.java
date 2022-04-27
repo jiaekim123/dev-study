0586b860d764462277f40f5be4bddefa8fae89e8
jiae <hirlawldo413@gmail.com>
2022-04-27 21:21:54 +0900

    [build] Spring 프로젝트 초기 셋팅

---
+++ src/test/java/com/study/devstudy/DevStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.devstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
