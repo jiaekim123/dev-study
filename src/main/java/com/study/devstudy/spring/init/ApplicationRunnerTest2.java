12e9d9399d2d0bef8610ccb392c78aa7debde27f
jiae <hirlawldo413@gmail.com>
2022-04-27 21:26:50 +0900

    [build] spring init (Runner, ApplicationReadyEvent) 테스트

---
+++ src/main/java/com/study/devstudy/spring/init/ApplicationRunnerTest2.java
@@ -0,0 +1,21 @@
+package com.study.devstudy.spring.init;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * 설명 :
+ *
+ * @author 김지애(Nova) / jiae.kim413@dreamus.io
+ * @since 2022/04/20
+ */
+@Slf4j
+@Component
+public class ApplicationRunnerTest2 implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("[ApplicationRunner] Test2");
+    }
+}
---
+++ src/main/java/com/study/devstudy/spring/init/CommandLineRunnerTest1.java
@@ -0,0 +1,21 @@
+package com.study.devstudy.spring.init;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * 설명 :
+ *
+ * @author 김지애(Nova) / jiae.kim413@dreamus.io
+ * @since 2022/04/20
+ */
+
+@Slf4j
+@Component
+public class CommandLineRunnerTest1 implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("[CommandLineRunner] Test1");
+    }
+}
